1f5ae0b4ecf42371b19e2cb475d59885cb8a3304
mohamed khaled ashour <mohamed.khaled@NassarGroup.local>
2025-03-13 11:59:44 +0200

    refactor: remove the auto spacing in the input

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 function formatSerial(input) {
   let value = input.value.replace(/\s+/g, "").toUpperCase(); // Remove spaces & ensure uppercase
-  let formattedValue = value.match(/.{1,4}/g)?.join(" ") || ""; // Insert space every 4 characters
+  // let formattedValue = value.match(/.{1,4}/g)?.join(" ") || ""; // Insert space every 4 characters
   input.value = formattedValue;
 }
