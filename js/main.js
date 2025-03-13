bfd55993c84e5236fac182be7a9e4762092ea416
mohamed khaled ashour <mohamed.khaled@NassarGroup.local>
2025-03-13 12:25:42 +0200

    fix: fix the remove spaces function

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 function formatSerial(input) {
   let value = input.value.replace(/\s+/g, "").toUpperCase(); // Remove spaces & ensure uppercase
   // let formattedValue = value.match(/.{1,4}/g)?.join(" ") || ""; // Insert space every 4 characters
-  input.value = formattedValue;
+  input.value = value;
 }
